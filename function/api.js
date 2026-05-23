396d29cf936478dc7457054709eebfe091b26cf1
geers <131465216+rrrrttt000555@users.noreply.github.com>
2026-05-23 20:34:36 +0500

    Update api.js

--- function/api.js
+++ function/api.js
@@ -203,9 +203,9 @@ router.post('/messages', async (req, res) => {
   } catch(e) { res.sendStatus(403); }
 });
 
-// Монтируем роутер по всем возможным путям
-app.use('/api', router);
+// Универсальный обработчик для Netlify
 app.use('/.netlify/functions/api', router);
-app.use('/', router); // Добавляем корень для надежности
+app.use('/api', router);
+app.use('/', router);
 
 export const handler = serverless(app);
